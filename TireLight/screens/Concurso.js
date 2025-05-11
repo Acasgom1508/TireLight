efb33d3844f176790912e3a7b44b87d1bf09cce1
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2025-05-11 21:34:56 +0200

    practicamente terminada la funcionalida de la pantalla concurso

--- TireLight/screens/Concurso.js
+++ TireLight/screens/Concurso.js
@@ -7,6 +7,9 @@ import {
   Image,
   TouchableOpacity,
   Dimensions,
+  Modal,
+  Alert,
+  ScrollView,
 } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
 import { doc, getDoc, collection, getDocs } from "firebase/firestore";
@@ -20,6 +23,13 @@ export default function Concurso() {
   const [nombre, setNombre] = useState(null);
   const [fotoPerfil, setFotoPerfil] = useState(null);
   const [tematica, setTematica] = useState(null);
+  const [antiguaTematica, setAntiguaTematica] = useState(null);
+  const [antiguoGanador, setAntiguoGanador] = useState(null);
+  const [imagenesTematica, setImagenesTematica] = useState([]);
+  const [bases, setBases] = useState(null);
+  const [votos, setVotos] = useState(null);
+  const [fecha, setFecha] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   // Firebase
   const auth = FIREBASE_AUTH;
@@ -52,24 +62,47 @@ export default function Concurso() {
 
   // Cargamos la temática actual
   useEffect(() => {
-    const cargarTematicasSeleccionadas = async () => {
+    const cargarTematicasYFotos = async () => {
       try {
-        const refCollection = collection(db, "Tematicas");
-        const querySnapshot = await getDocs(refCollection);
-        const tematicasSeleccionadas = querySnapshot.docs
-          .filter((doc) => doc.data().Seleccionado === "Si")
-          .map((doc) => ({ id: doc.id, ...doc.data() }));
-        setTematica(tematicasSeleccionadas);
+        const querySnapshot = await getDocs(collection(db, "Tematicas"));
+        let tematicaActual = null;
+
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          if (data.Seleccionado === "Si") {
+            setTematica(data.Titulo);
+            setBases(data.Bases);
+            tematicaActual = data.Titulo;
+          } else if (data.Seleccionado === "AG") {
+            setAntiguaTematica(data.Titulo);
+          }
+        });
+
+        // Si se ha encontrado la temática actual, cargar fotos
+        if (tematicaActual) {
+          const fotosSnapshot = await getDocs(collection(db, "Fotos"));
+
+          const imagenesFiltradas = fotosSnapshot.docs
+            .filter((doc) => doc.data().Tematica === tematicaActual)
+            .map((doc) => {
+              const data = doc.data();
+              return {
+                url: data.Url,
+                titulo: data.Titulo,
+                fecha: data.Fecha,
+                votos: data.Votos,
+              };
+            });
+
+          setImagenesTematica(imagenesFiltradas);
+        }
       } catch (error) {
-        console.error("Error al cargar las temáticas seleccionadas:", error);
-        Alert.alert(
-          "Error",
-          "No se pudieron cargar las temáticas seleccionadas."
-        );
+        console.error("Error al cargar datos:", error);
+        Alert.alert("Error", "No se pudieron cargar las temáticas o imágenes.");
       }
     };
 
-    cargarTematicasSeleccionadas();
+    cargarTematicasYFotos();
   }, []);
 
   return (
@@ -88,11 +121,12 @@ export default function Concurso() {
               fotoPerfil ||
               "https://i.pinimg.com/222x/57/70/f0/5770f01a32c3c53e90ecda61483ccb08.jpg",
           }}
-          style={styles.image}
+          style={styles.imagen}
         />
         <Text style={styles.bienvenida}>Bienvenido {nombre}</Text>
       </View>
 
+      {/* Boton añadir foto */}
       <TouchableOpacity
         style={styles.botonAnnadir}
         onPress={() => navigation.navigate("AnnadirFoto")}
@@ -100,13 +134,79 @@ export default function Concurso() {
         <Feather name="plus" size={25} color="#fff" />
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.botonBases}>
-        <Feather name="info" size={25} color="#1E205B" />
+      {/* Boton ver bases */}
+      <TouchableOpacity
+        style={styles.botonBases}
+        onPress={() => setModalVisible(true)}
+      >
+        <Feather name="info" size={35} color="#1E205B" />
       </TouchableOpacity>
 
-      {/* Antiguo ganador */}
+      {/* Modal que se ve al darle al boton de información */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.contenedorModal}>
+          <View style={styles.contenidoModal}>
+            <Text style={styles.tituloModal}>Bases de la temática actual</Text>
+            <Text style={styles.textoModal}>
+              {bases || "No hay bases disponibles."}
+            </Text>
+            <TouchableOpacity
+              style={styles.botonCerrar}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textoBotonCerrar}>Cerrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
 
-      {/* Temática actual */}
+      <ScrollView style={styles.scrollViewFotos}>
+        {/* Antiguo ganador */}
+        <View style={styles.encabezado}>
+          <Text style={styles.textoEncabezado}>
+            Temática anterior: {antiguaTematica}
+          </Text>
+        </View>
+
+        {/* Temática actual */}
+        <View style={styles.encabezado}>
+          <Text style={styles.textoEncabezado}>
+            Temática actual: {tematica}
+          </Text>
+        </View>
+        {imagenesTematica.length > 0 ? (
+          imagenesTematica.map((foto, index) => (
+            <View style={styles.fotoContainer} key={index}>
+              <Image
+                source={{ uri: foto.url }}
+                style={{
+                  width: width * 0.75,
+                  height: height * 0.2,
+                  borderRadius: 10,
+                  marginBottom: 10,
+                  alignSelf: "center",
+                  borderWidth: 1,
+                  borderColor: "#9B9B9B",
+                }}
+              />
+              <Text style={{ fontWeight: "bold" }}>Título: {foto.titulo}</Text>
+              <Text>Fecha: {foto.fecha}</Text>
+              <Text>Votos: {foto.votos}</Text>
+            </View>
+          ))
+        ) : (
+          <Text style={{ textAlign: "center", marginTop: 10 }}>
+            No hay imágenes para esta temática.
+          </Text>
+        )}
+      </ScrollView>
 
       <StatusBar style="auto" />
     </View>
@@ -121,12 +221,7 @@ const styles = StyleSheet.create({
     marginTop: height * 0.06,
   },
 
-  botonAtras: {
-    position: "absolute",
-    left: width * 0.1,
-  },
-
-  header: {
+  encabezado: {
     flexDirection: "row",
     width: "100%",
     height: height * 0.07,
@@ -134,14 +229,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 
-  textoHeader: {
+  textoEncabezado: {
     fontSize: width * 0.05,
   },
 
-  image: {
+  imagen: {
     width: width * 0.15,
     height: width * 0.15,
-    marginBottom: height * 0.02,
     borderRadius: (width * 0.5) / 2,
     borderWidth: 1,
     borderColor: "#9B9B9B",
@@ -151,7 +245,6 @@ const styles = StyleSheet.create({
   bienvenida: {
     fontSize: width * 0.05,
     color: "#1E205B",
-    marginBottom: height * 0.02,
   },
 
   botonAnnadir: {
@@ -169,9 +262,68 @@ const styles = StyleSheet.create({
 
   botonBases: {
     position: "absolute",
-    top: height * 0.04,
-    right: width * 0.14,
+    top: height * 0.037,
+    right: width * 0.09,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  contenedorModal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+
+  contenidoModal: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  tituloModal: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  textoModal: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+
+  botonCerrar: {
+    backgroundColor: "#1E205B",
+    borderRadius: 5,
+    padding: 10,
+  },
+
+  textoBotonCerrar: {
+    color: "white",
+    fontSize: 16,
+  },
+
+  scrollViewFotos: {
+    alignSelf: "center",
+    paddingHorizontal: width * 0.03,
+    marginVertical: height * 0.02,
+  },
+
+  fotoContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 15,
+    alignItems: "center",
+    marginBottom: 15,
   },
 });
